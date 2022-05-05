6653362523e29215a81ea9b39db193bbca8cf6f9
Glen1986 <escobar.glen@gmail.com>
2022-05-05 01:11:15 -0300

    react-router-dom-rutas-variables

--- src/2-nested.js
+++ src/2-nested.js
@@ -1,29 +1,23 @@
-import { Switch, Route, Link, useRouteMatch, useParams } from 'react-router-dom'
-
-const Proyecto = () => {
-    const match = useRouteMatch()
-    const params = useParams()
-    console.log({ match, params })
-    const { proyecto_id } = match.params
-    return <h1>proyecto {proyecto_id}</h1>
-}
+import { Switch, Route, Link } from 'react-router-dom'
 const Portafolio = () => {
-    const match = useRouteMatch()
     return (
         <div>
             <h1>Portafolio</h1>
             <ul>
                 <li>
-                    <Link to={`${match.url}/proyecto1`}>Proyecto1</Link>
+                    <Link to="/portafolio/proyecto1">Proyecto1</Link>
                 </li>
                 <li>
-                    <Link to={`${match.url}/proyecto2`}>Proyecto2</Link>
+                    <Link to="/portafolio/proyecto2">Proyecto2</Link>
                 </li>
             </ul>
             <div>
                 <Switch>
-                    <Route path={`${match.path}/:proyecto_id`}>
-                        <Proyecto />
+                    <Route path="/portafolio/proyecto1">
+                        <h2>proyecto 1</h2>
+                    </Route>
+                    <Route path="/portafolio/proyecto2">
+                        <h2>proyecto 2</h2>
                     </Route>
                 </Switch>
             </div>
